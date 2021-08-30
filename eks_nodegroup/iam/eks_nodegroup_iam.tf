3b4d31b120de6dae67671609f1646f7a76540260
rohan <rohan.gupta@telekom-digital.com>
2021-08-30 21:45:22 +0530

    added iam policy in nodegroup

--- eks_nodegroup/iam/eks_nodegroup_iam.tf
+++ eks_nodegroup/iam/eks_nodegroup_iam.tf
@@ -6,118 +6,79 @@ resource "aws_iam_policy" "eks_worker_node_PolicyALBingressController" {
 
   policy = <<EOF
 {
+
   "Version": "2012-10-17",
   "Statement": [
       {
-          "Effect": "Allow",
-          "Action": [
-              "acm:DescribeCertificate",
-              "acm:ListCertificates",
-              "acm:GetCertificate"
-          ],
-          "Resource": "*"
-      },
-      {
+          "Sid": "VisualEditor0",
           "Effect": "Allow",
           "Action": [
               "ec2:AuthorizeSecurityGroupIngress",
-              "ec2:CreateSecurityGroup",
-              "ec2:CreateTags",
-              "ec2:DeleteTags",
-              "ec2:DeleteSecurityGroup",
-              "ec2:DescribeAccountAttributes",
-              "ec2:DescribeAddresses",
-              "ec2:DescribeInstances",
-              "ec2:DescribeInstanceStatus",
-              "ec2:DescribeInternetGateways",
-              "ec2:DescribeNetworkInterfaces",
-              "ec2:DescribeSecurityGroups",
-              "ec2:DescribeSubnets",
-              "ec2:DescribeTags",
-              "ec2:DescribeVpcs",
-              "ec2:ModifyInstanceAttribute",
-              "ec2:ModifyNetworkInterfaceAttribute",
-              "ec2:RevokeSecurityGroupIngress"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
-              "elasticloadbalancing:AddListenerCertificates",
-              "elasticloadbalancing:AddTags",
-              "elasticloadbalancing:CreateListener",
-              "elasticloadbalancing:CreateLoadBalancer",
-              "elasticloadbalancing:CreateRule",
-              "elasticloadbalancing:CreateTargetGroup",
-              "elasticloadbalancing:DeleteListener",
-              "elasticloadbalancing:DeleteLoadBalancer",
-              "elasticloadbalancing:DeleteRule",
-              "elasticloadbalancing:DeleteTargetGroup",
-              "elasticloadbalancing:DeregisterTargets",
-              "elasticloadbalancing:DescribeListenerCertificates",
-              "elasticloadbalancing:DescribeListeners",
-              "elasticloadbalancing:DescribeLoadBalancers",
-              "elasticloadbalancing:DescribeLoadBalancerAttributes",
-              "elasticloadbalancing:DescribeRules",
-              "elasticloadbalancing:DescribeSSLPolicies",
-              "elasticloadbalancing:DescribeTags",
-              "elasticloadbalancing:DescribeTargetGroups",
-              "elasticloadbalancing:DescribeTargetGroupAttributes",
-              "elasticloadbalancing:DescribeTargetHealth",
               "elasticloadbalancing:ModifyListener",
-              "elasticloadbalancing:ModifyLoadBalancerAttributes",
-              "elasticloadbalancing:ModifyRule",
-              "elasticloadbalancing:ModifyTargetGroup",
-              "elasticloadbalancing:ModifyTargetGroupAttributes",
+              "ec2:DescribeInstances",
+              "iam:ListServerCertificates",
               "elasticloadbalancing:RegisterTargets",
-              "elasticloadbalancing:RemoveListenerCertificates",
-              "elasticloadbalancing:RemoveTags",
               "elasticloadbalancing:SetIpAddressType",
-              "elasticloadbalancing:SetSecurityGroups",
-              "elasticloadbalancing:SetSubnets",
-              "elasticloadbalancing:SetWebACL"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
-              "iam:CreateServiceLinkedRole",
-              "iam:GetServerCertificate",
-              "iam:ListServerCertificates"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
-              "cognito-idp:DescribeUserPoolClient"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
+              "ec2:DescribeInternetGateways",
+              "elasticloadbalancing:DeleteLoadBalancer",
+              "elasticloadbalancing:SetWebACL",
+              "elasticloadbalancing:SetWebAcl",
               "waf-regional:GetWebACLForResource",
+              "elasticloadbalancing:DescribeLoadBalancers",
+              "acm:GetCertificate",
               "waf-regional:GetWebACL",
+              "elasticloadbalancing:CreateRule",
+              "ec2:DescribeAccountAttributes",
+              "elasticloadbalancing:AddListenerCertificates",
+              "elasticloadbalancing:ModifyTargetGroupAttributes",
+              "waf:GetWebACL",
+              "iam:GetServerCertificate",
+              "ec2:CreateTags",
+              "ec2:ModifyNetworkInterfaceAttribute",
+              "elasticloadbalancing:CreateTargetGroup",
+              "elasticloadbalancing:DeregisterTargets",
+              "ec2:RevokeSecurityGroupIngress",
+              "elasticloadbalancing:DescribeLoadBalancerAttributes",
+              "elasticloadbalancing:DescribeTargetGroupAttributes",
+              "acm:DescribeCertificate",
+              "elasticloadbalancing:ModifyRule",
+              "elasticloadbalancing:AddTags",
+              "elasticloadbalancing:DescribeRules",
+              "ec2:DescribeSubnets",
+              "elasticloadbalancing:ModifyLoadBalancerAttributes",
               "waf-regional:AssociateWebACL",
-              "waf-regional:DisassociateWebACL"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
+              "ec2:DescribeAddresses",
               "tag:GetResources",
-              "tag:TagResources"
-          ],
-          "Resource": "*"
-      },
-      {
-          "Effect": "Allow",
-          "Action": [
-              "waf:GetWebACL"
+              "ec2:DeleteTags",
+              "elasticloadbalancing:RemoveListenerCertificates",
+              "tag:TagResources",
+              "elasticloadbalancing:RemoveTags",
+              "elasticloadbalancing:CreateListener",
+              "ec2:DescribeNetworkInterfaces",
+              "elasticloadbalancing:DescribeListeners",
+              "ec2:CreateSecurityGroup",
+              "acm:ListCertificates",
+              "elasticloadbalancing:DescribeListenerCertificates",
+              "ec2:ModifyInstanceAttribute",
+              "cognito-idp:DescribeUserPoolClient",
+              "elasticloadbalancing:DeleteRule",
+              "ec2:DescribeInstanceStatus",
+              "elasticloadbalancing:DescribeSSLPolicies",
+              "elasticloadbalancing:CreateLoadBalancer",
+              "waf-regional:DisassociateWebACL",
+              "ec2:DescribeTags",
+              "elasticloadbalancing:DescribeTags",
+              "elasticloadbalancing:SetSubnets",
+              "elasticloadbalancing:DeleteTargetGroup",
+              "ec2:DescribeSecurityGroups",
+              "iam:CreateServiceLinkedRole",
+              "ec2:DescribeVpcs",
+              "ec2:DeleteSecurityGroup",
+              "elasticloadbalancing:DescribeTargetHealth",
+              "elasticloadbalancing:SetSecurityGroups",
+              "elasticloadbalancing:DescribeTargetGroups",
+              "elasticloadbalancing:ModifyTargetGroup",
+              "elasticloadbalancing:DeleteListener"
           ],
           "Resource": "*"
       }
